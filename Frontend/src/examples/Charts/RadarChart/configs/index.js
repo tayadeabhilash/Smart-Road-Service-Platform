8bd43db9eb1125eb0c499a33bc4605d4f9d57791
Abhilash <tayadeabhilash@gmail.com>
2024-11-10 13:53:49 -0800

    First Commit

---
+++ Frontend/src/examples/Charts/RadarChart/configs/index.js
@@ -0,0 +1,25 @@
+
+
+function configs(labels, datasets) {
+  return {
+    data: {
+      labels,
+      datasets: [...datasets],
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      plugins: {
+        legend: {
+          display: false,
+        },
+      },
+      interaction: {
+        intersect: false,
+        mode: "index",
+      },
+    },
+  };
+}
+
+export default configs;
